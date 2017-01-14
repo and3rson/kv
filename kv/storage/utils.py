42ab065920889087c21be41ce26148a570c46c45
Andrew Dunai <andunai@gmail.com>
2017-01-14 03:27:49 +0200

    Fix

--- kv/storage/utils.py
+++ kv/storage/utils.py
@@ -6,8 +6,12 @@ import re
 def yaml_response(fn):
     def wrapper(*args, **kwargs):
         result = fn(*args, **kwargs)
+        if isinstance(result, (str, unicode)):
+            data = result
+        else:
+            data = re.sub('\n\.\.\.$', '', yaml.safe_dump(result, default_flow_style=False)),
         return HttpResponse(
-            re.sub('\n\.\.\.$', '', yaml.safe_dump(result, default_flow_style=False)),
+            data,
             content_type='text/plain'
         )
     return wrapper
